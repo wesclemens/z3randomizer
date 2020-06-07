65db6eefb7b21ad5386f92ca461572ad9636587a
caitsith2 <d_good@caitsith2.com>
2020-06-07 05:03:42 -0700

    update windows build process to asar

--- build.bat
+++ build.bat
@@ -1,4 +1,4 @@
 del ..\working.sfc
 copy ..\alttp.sfc ..\working.sfc
-xkas.exe LTTP_RND_GeneralBugfixes.asm ..\working.sfc
+asar.exe LTTP_RND_GeneralBugfixes.asm ..\working.sfc
 @echo %cmdcmdline%|find /i """%~f0""">nul && cmd /k
