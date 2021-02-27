3ce0df78798b5118509776e7b89be4a7e70a5750
pepperpow <pepe.tjg@gmail.com>
2021-02-27 03:21:03 -0600

    Properly remove overlay when in Misery Mire post zelda

--- removerain.asm
+++ removerain.asm
@@ -5,14 +5,16 @@ Overworld_RemoveRainLong:
     +
 
     LDA $8A : CMP #$70 : BNE + 
-        LDA $7EF2F0 : AND.b #$20 : BNE .endNoRain
-        LDA $0410 : ORA $0416 : ORA $0418 : BNE .endNoRain ; if we're transitioning at a direction
+        LDA $7EF2F0 : AND.b #$20 : BNE .endNoRainClearOverlay
+        LDA $0410 : ORA $0416 : ORA $0418 : BNE .endNoRainClearOverlay ; if we're transitioning at a direction
             BRA .endWithRain
     +
 
     LDA $7EF3C5 : CMP.b #$02 : !BGE .endNoRain
     .endWithRain
     LDA #$FF : RTL
+    .endNoRainClearOverlay
+    STZ $1D
     .endNoRain
     LDA #$00 : RTL
 }
